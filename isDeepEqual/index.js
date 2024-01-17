2ff9807dd653c9e275d1f8f63c1bfeb4fbb4435a
shhumen <mehtiyevashumen@gmail.com>
2024-01-17 23:58:18 +0400

    fix

--- isDeepEqual/index.js
+++ isDeepEqual/index.js
@@ -26,6 +26,11 @@ const getIsDeepEqual = function (obj1, obj2) {
   }
 };
 
-console.log(getIsDeepEqual({ a: undefined, b: 2, test: [1, 2, 3], obj: { test: 13 } }, { a: undefined, b: 2, test: [1, 2, 3], obj: { test: 13 } }));
-
-
+console.log(getIsDeepEqual({ a: 1, b: 2 }, { a: 1, b: 3 })); // false
+console.log(getIsDeepEqual({ a: 1, b: 2 }, { a: 1, b: 2 })); // true
+console.log(getIsDeepEqual( { a: 1, b: 2, obj: { test: "12" } }, { a: 1, b: 2, obj: { test: "12" } } ));  // true
+console.log(getIsDeepEqual( { at: 1, bt: 2, test: [1,2,3], obj: { test: "12" } } , { at: 1, bt: 2, test: [1,2,3], obj: { test: "12", } })); // true 
+console.log(getIsDeepEqual( { at: 1, bt: 2, test: [1,2], obj: { test: "12" } } , { at: 1, bt: 2, test: [1,2,3], obj: { test: "12" } }));  // false
+console.log(getIsDeepEqual( { at: 1, bt: 2, test: [1, 2, 3, { hi: "hi"} ], obj: { test: "12" } } , { at: 1, bt: 2, test: [1, 2, 3, { hi: "hello"} ], obj: { test: "12" } } )); // false
+console.log(getIsDeepEqual( { at: 1, test: [1, 2, 3, { hi: "hello"}, null ], obj: { test: "12" } } , { at: 1, test: [1, 2, 3, { hi: "hello"}, null ], obj: { test: "12" } } )); // true
+console.log(getIsDeepEqual( {  test: [1, 2, 3, { hi: "hello"}, undefined ], obj: { test: "12" } } , { test: [1, 2, 3, { hi: "hello"}, null ], obj: { test: "12" } } )); // false
